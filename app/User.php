55c515d479fb08682636c1c73e04e589bc0abd95
mhsnawan <mohsin.awan0@gmail.com>
2018-11-25 15:47:12 +0500

    first commit

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    // public function conversation_users(){
+    //     return $this->hasMany('App\ConversationUser');
+    // }
+
+    public function conversations() {
+        return $this->belongsToMany('App\Conversations', 'conversation_users', 'user_id', 'conversation_id');
+    }
+
+
+    public function messages(){
+        return $this->hasMany('App\Messages', 'conversation_id', 'id');
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+<h1>Laravel Chat App</h1>
+
+To run this project:
+1. Clone this to your PC.
+2. Setup .env file.
+3. Run <code>php artisan key:generate</code>
+5. Run <code>composer install</code> to install all packages in the terminal.
+6. Run <code>php artisan migrate</code>
+
+<h2>Routes</h2>
+1. /register and /login to register and login.<br>
+2. /sendchat to see all users to whom you want to send message.
+<br>
+
+<p>This code has copyrighs © <b>Mohsin Awan</b> </p>
